1df356d065f6a036e79e70f0122cd79a0152ba08
Andrew Sosa Guaita <85213217+andrewsosag@users.noreply.github.com>
2023-12-18 23:14:04 -0800

    changing to MySQL database

--- app.py
+++ app.py
@@ -1,44 +1,52 @@
 import os
+import mysql.connector
+import json
 import requests
-import psycopg2
-from psycopg2 import extras
 import datetime
 import boto3
-import json
+import logging
 
-# Fetching values from environment variables
-# SEATGEEK_CLIENT_ID = os.environ.get('SEATGEEK_CLIENT_ID')
-# SEATGEEK_CLIENT_SECRET = os.environ.get('SEATGEEK_CLIENT_SECRET')
-# DATABASE_URL = os.environ.get('DATABASE_URL')
-# Super top secret
+def lambda_handler(event, context):
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Starting script...")
+    
+    # Fetch values from environment variables
+    SEATGEEK_CLIENT_ID = os.environ.get('SEATGEEK_CLIENT_ID')
+    RDS_HOST = os.environ.get('RDS_HOST')
+    RDS_USER = os.environ.get('RDS_USER')
+    RDS_PASSWORD = os.environ.get('RDS_PASSWORD')
+    RDS_DB_NAME = os.environ.get('RDS_DB_NAME')
 
-def get_secret(secret_name):
-    client = boto3.client('secretsmanager')
-    try:
-        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-    except Exception as e:
-        raise e
-    else:
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-            return json.loads(secret)
-        else:
-            raise Exception("Secret not found")
-        
-secrets = get_secret("MyDatabaseCredentials")
-SEATGEEK_CLIENT_ID = secrets['SEATGEEK_CLIENT_ID']
-SEATGEEK_CLIENT_SECRET = secrets['SEATGEEK_CLIENT_SECRET']
-DATABASE_URL = secrets['DATABASE_URL']
+    # Database creation
+    logging.info("Creating database...")
+    create_database(RDS_HOST, RDS_USER, RDS_PASSWORD, RDS_DB_NAME)
 
-def fetch_events():
+    # Fetching events
+    logging.info("Fetching events...")
+    events = fetch_events(SEATGEEK_CLIENT_ID)
+
+    # Parsing and updating events
+    logging.info("Parsing and updating events...")
+    parsed_data = parse_event_data(events)
+    for event in parsed_data:
+        insert_or_update_event(event, RDS_HOST, RDS_USER, RDS_PASSWORD, RDS_DB_NAME)
+
+    logging.info("Script completed successfully.")
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Data update successful!')
+    }
+
+
+def fetch_events(client_id):
     api_url = 'https://api.seatgeek.com/2/events'
     all_events = []
     page = 1
-    while len(all_events) < 5000:
+    while len(all_events) < 30:  # Limiting to 30 events for testing
         params = {
-            'client_id': SEATGEEK_CLIENT_ID,
+            'client_id': client_id,
             'taxonomies.name': 'concert',
-            'per_page': 500,
+            'per_page': 10,  # Reduced for testing
             'sort': 'score.desc',
             'page': page
         }
@@ -47,19 +55,16 @@ def fetch_events():
             print(f"Failed to fetch events: {response.status_code}, Response: {response.text}")
             break
         events = response.json()['events']
-        print(f"Fetched page {page} with {len(events)} events.")
         if not events:
             break
         all_events.extend(events)
         page += 1
-    return all_events[:5000]  # Limit to first 5000 events
-
+    return all_events
 
 def parse_event_data(events):
     parsed_data = []
     for event in events:
-        event_id = str(event['id']) # Assuming 'id' is the unique identifier provided by the API
-        # event_id = event['id'] 
+        event_id = str(event['id'])
         event_name = event['title']
         event_date = event['datetime_local']
         venue_name = event['venue']['name']
@@ -68,7 +73,7 @@ def parse_event_data(events):
         highest_price = event['stats'].get('highest_price', 'N/A')
 
         parsed_data.append({
-            'event_id': event_id,  # Include event_id in the dictionary
+            'event_id': event_id,
             'event_name': event_name,
             'event_date': event_date,
             'venue_name': venue_name,
@@ -76,37 +81,31 @@ def parse_event_data(events):
             'lowest_price': lowest_price,
             'highest_price': highest_price
         })
-
     return parsed_data
 
-
-def create_database():
-    conn = psycopg2.connect(DATABASE_URL)
+def create_database(rds_host, rds_user, rds_password, rds_db_name):
+    conn = mysql.connector.connect(user=rds_user, password=rds_password, 
+                                   host=rds_host, 
+                                   database=rds_db_name)
     cur = conn.cursor()
 
-    # Drop existing tables if they exist
-    cur.execute("DROP TABLE IF EXISTS event_prices CASCADE")
-    cur.execute("DROP TABLE IF EXISTS events CASCADE")
-
-    # Create events table
     cur.execute('''
-        CREATE TABLE events (
-            id SERIAL PRIMARY KEY,
-            event_id TEXT UNIQUE NOT NULL,
-            event_name TEXT,
-            event_date TEXT,
-            venue_name TEXT,
-            city_name TEXT
+        CREATE TABLE IF NOT EXISTS events (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            event_id VARCHAR(255) UNIQUE NOT NULL,
+            event_name VARCHAR(255),
+            event_date DATETIME,
+            venue_name VARCHAR(255),
+            city_name VARCHAR(255)
         )
     ''')
 
-    # Create event_prices table
     cur.execute('''
-        CREATE TABLE event_prices (
-            event_id TEXT NOT NULL,
+        CREATE TABLE IF NOT EXISTS event_prices (
+            event_id VARCHAR(255) NOT NULL,
             date DATE NOT NULL,
-            lowest_price REAL,
-            highest_price REAL,
+            lowest_price FLOAT,
+            highest_price FLOAT,
             PRIMARY KEY (event_id, date),
             FOREIGN KEY (event_id) REFERENCES events(event_id)
         )
@@ -114,31 +113,27 @@ def create_database():
     conn.commit()
     conn.close()
 
-
-def insert_or_update_event(event):
-    conn = psycopg2.connect(DATABASE_URL)
+def insert_or_update_event(event, rds_host, rds_user, rds_password, rds_db_name):
+    conn = mysql.connector.connect(user=rds_user, password=rds_password, 
+                                   host=rds_host, 
+                                   database=rds_db_name)
     cur = conn.cursor()
 
-    # Check if the event already exists in 'events' table
     cur.execute("SELECT * FROM events WHERE event_id = %s", (event['event_id'],))
     if not cur.fetchone():
-        # Insert a new event
         cur.execute('''
             INSERT INTO events (event_id, event_name, event_date, venue_name, city_name)
             VALUES (%s, %s, %s, %s, %s)
         ''', (event['event_id'], event['event_name'], event['event_date'], event['venue_name'], event['city_name']))
 
-    # Update or insert into 'event_prices' table
     cur.execute("SELECT * FROM event_prices WHERE event_id = %s AND date = CURRENT_DATE", (event['event_id'],))
     if cur.fetchone():
-        # Update the existing price data
         cur.execute('''
-            UPDATE event_prices 
-            SET lowest_price = %s, highest_price = %s 
+            UPDATE event_prices
+            SET lowest_price = %s, highest_price = %s
             WHERE event_id = %s AND date = CURRENT_DATE
         ''', (event['lowest_price'], event['highest_price'], event['event_id']))
     else:
-        # Insert new price data
         cur.execute('''
             INSERT INTO event_prices (event_id, date, lowest_price, highest_price)
             VALUES (%s, CURRENT_DATE, %s, %s)
@@ -147,100 +142,15 @@ def insert_or_update_event(event):
     conn.commit()
     conn.close()
 
-
-def run_query(query):
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-    cur.execute(query)
-    results = cur.fetchall()
-    conn.close()
-    return results
-
-
-def clear_data():
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-    cur.execute('DELETE FROM events')
-    conn.commit()
-    conn.close()
-
-
-def delete_past_events():
-    """
-    Delete events that have already occurred.
-    """
-    today = datetime.datetime.now().strftime("%Y-%m-%d")  # Keep today's concerts
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-    cur.execute("DELETE FROM events WHERE event_date < %s", (today,))
-    conn.commit()
-    conn.close()
-
-
-def clear_old_price_data():
-    """
-    Clear price data that is older than 90 days.
-    """
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-    ninety_days_ago = (datetime.datetime.now() - datetime.timedelta(days=90)).strftime("%Y-%m-%d")
-    cur.execute("DELETE FROM event_prices WHERE date < %s", (ninety_days_ago,))
-    conn.commit()
-    conn.close()
-
-
-def delete_past_events():
-    """
-    Delete events that have already occurred.
-    """
-    today = datetime.datetime.now().strftime("%Y-%m-%d")  # Keep today's concerts
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-    cur.execute("DELETE FROM events WHERE event_date < %s", (today,))
-    conn.commit()
-    conn.close()
-
-
-def batch_update_events(parsed_data):
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-
-    # Prepare batch data for events
-    events_batch = [(e['event_id'], e['event_name'], e['event_date'], e['venue_name'], e['city_name']) for e in parsed_data]
-    # Prepare batch data for event prices
-    prices_batch = [(e['event_id'], datetime.date.today(), e['lowest_price'], e['highest_price']) for e in parsed_data]
-
-    # Batch insert events
-    psycopg2.extras.execute_batch(cur, """
-        INSERT INTO events (event_id, event_name, event_date, venue_name, city_name) 
-        VALUES (%s, %s, %s, %s, %s)
-        ON CONFLICT (event_id) DO NOTHING
-    """, events_batch)
-
-    # Batch insert or update prices
-    psycopg2.extras.execute_batch(cur, """
-        INSERT INTO event_prices (event_id, date, lowest_price, highest_price) 
-        VALUES (%s, %s, %s, %s)
-        ON CONFLICT (event_id, date) 
-        DO UPDATE SET lowest_price = EXCLUDED.lowest_price, highest_price = EXCLUDED.highest_price
-    """, prices_batch)
-
-    conn.commit()
-    cur.close()
-    conn.close()
-    print(f"Processed batch of {len(parsed_data)} events.")
-
-
-def main(event, context):
+def main():
     print("Starting script...")
     create_database()
     events = fetch_events()
-    if events:
-        parsed_data = parse_event_data(events)
-        batch_update_events(parsed_data)
-        delete_past_events()
-        clear_old_price_data()
+    parsed_data = parse_event_data(events)
+    for event in parsed_data:
+        insert_or_update_event(event)
+
     print("Script completed successfully.")
 
 if __name__ == '__main__':
-    main(event=None, context=None)
+    lambda_handler(None, None)
